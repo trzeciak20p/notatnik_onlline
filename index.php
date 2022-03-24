bde2950f775e7e2b16098fc167afc750d0fe168c
trzeciak20p <m.trzeciak.p20@gmail.com>
2022-03-24 16:23:28 +0100

    struktura base

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OnPaint</title>
+    <link src="style.css" rel="stylesheet">
+</head>
+<body>
+
+    <nav>
+        <ul>
+            <li>Strona Główna</li>
+            <li>Twoje Notatki</li>
+            <li>Logowanie</li>
+        </ul>
+    </nav>
+
+    <header>
+        <label>
+            Wyszukaj <input type="text">
+        </label>
+    </header>
+
+    <article>
+        <h2>Obserwowani</h2>
+
+    </article>
+
+    <article>
+        <h2>Ostatnie</h2>
+
+
+
+
+    </article>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <nav>   
+        <ul>
+            <a href="index.php"></a><li>Strona Główna</li></a>
+            <a href="notes.php"></a><li>Twoje Notatki</li></a>
+            <a href="login.php"><li>Logowanie</li></a>
+        </ul>
+    </nav>
+    
+    <main>
+
+
+
+
+
+    </main>
+
+</body>
+</html>
