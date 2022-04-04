1bba2b11d5c2c37263b3c2b006e50c15534f5cee
trzeciak20p <91263438+trzeciak20p@users.noreply.github.com>
2022-04-04 10:53:58 +0200

    Update forum.php

--- forum.php
+++ forum.php
@@ -7,8 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OnPaint</title>
-    <link src="style.css" rel="stylesheet">
-    <link href="img/icon" rel="icon" sizes="32X32">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="img/icon.png" sizes="32X32">
 </head>
 
     <nav>
