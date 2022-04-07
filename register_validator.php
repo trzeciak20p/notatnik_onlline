7f6e491792297342973fab512313f9709dc7ac05
trzeciak20p <91263438+trzeciak20p@users.noreply.github.com>
2022-04-07 11:23:07 +0200

    login/register validation

--- register_validator.php
+++ register_validator.php
@@ -1,20 +1,26 @@
 <?php
+ob_start();
 
-if( !isset($_POST["submit"]) ){     //sprawdzanie czy się weszło przez login.php
+if( !isset($_POST["submit"]) ){         //validacja
     echo "Nie łądnie tak wchodzić bez logowania!";
-    header("refresh:5; url=register.php");  
+    header("refresh:3; url=register.php");  
 }
 if( empty($_POST["login"]) || empty($_POST["password"]) || empty($_POST["password2"]) ){
     echo "Ale podaj wszystko może";
-    header("refresh:5; url=register.php");  
+    header("refresh:3; url=register.php");  
 }
 if( empty($_POST["captcha"]) ){
     echo "A przestań być robotem B(";
-    header("refresh:5; url=register.php");  
+    header("refresh:3; url=register.php");  
+}
+if( $_POST["password"] != $_POST["password2"] ){
+    echo "Podane hasła nie są takie same!";
+    header("refresh:3; url=register.php");  
 }
 
+//zapisywanie do pliku
+fopen("data/users.txt")
 
 
-
-header("Location: login.php");  
+// header("Location: login.php");  
 exit;
