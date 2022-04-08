17efd28aa6a13da109d4fa7bd32202b3281d516b
trzeciak20p <m.trzeciak.p20@gmail.com>
2022-04-08 16:06:08 +0200

    nav login css

--- nav_login_icon.php
+++ nav_login_icon.php
@@ -2,7 +2,7 @@
 session_start();
 
 if( isset($_SESSION["login"]) ){
-    echo "Witaj " . $_SESSION["login"];
+    echo "<a href='login.php'>Witaj " . $_SESSION["login"]. "</a>";
 }else{
     echo "<a href='login.php'>Zaloguj się</a>";
 }
