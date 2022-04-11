31f11c77634f7e63006318f65a7aafd19786d4bc
trzeciak20p <91263438+trzeciak20p@users.noreply.github.com>
2022-04-11 11:10:55 +0200

    imie capitalize

--- nav_login_icon.php
+++ nav_login_icon.php
@@ -2,7 +2,7 @@
 session_start();
 
 if( isset($_SESSION["login"]) ){
-    echo "<a href='login.php'>Witaj " . $_SESSION["login"]. "</a>";
+    echo "<a href='login.php'>Witaj <span style='text-transform: capitalize;'>" . $_SESSION["login"]. "</span></a>";
 }else{
     echo "<a href='login.php'>Zaloguj się</a>";
 }
