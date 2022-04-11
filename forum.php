e1669450c3f630115b87f03a7f7e39a5f6a0a973
trzeciak20p <91263438+trzeciak20p@users.noreply.github.com>
2022-04-11 11:03:15 +0200

    nav update

--- forum.php
+++ forum.php
@@ -17,7 +17,9 @@
             <a href="forum.php"><li>Prace</li></a>
             <a href="projects.php"><li>Twoje Projekty</li></a>
         </ul>
-        <a href="login.php">Zaloguj się</a>
+        <?php 
+        require('nav_login_icon.php'); 
+        ?>
     </nav>
 
     <main>
