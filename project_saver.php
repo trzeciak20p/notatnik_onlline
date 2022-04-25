4af797893a77ea03d7331a100174a6f0996e23f5
trzeciak20p <91263438+trzeciak20p@users.noreply.github.com>
2022-04-25 12:47:23 +0200

    Add files via upload

--- project_saver.php
+++ project_saver.php
@@ -1,16 +1,17 @@
 <?php
-error_reporting(0);
+// error_reporting(0);
+echo $_POST["canvas"];
 
 $save = $_POST["canvas"];
 // $f = fopen("data/" . $_SESSION["login"]. "/". $_POST["open_project"]. "/canvas1.jpg", "r");
-$dir = "data/" . $_SESSION["login"]. "/ae/canvas1.txt";
+$dir = "data/" . $_SESSION["login"]. "/ae/canvas1.txt"; //
 echo $dir;
-$f = fopen($dir, "a");
-fwrite($f, "japko");
-echo $_POST["canvas"];
+$f = fopen($dir, "w+");
+fwrite($f, $_POST["canvas"]);
+
 
 
 
 
 ?>
-<div id='canvas_save' style='background-color: red;'> <?php @$_POST["canvas"] ?> </div>
+<div id='canvas_save' style='background-color: red;'> <?php echo @$_POST["canvas"] ?> </div>
