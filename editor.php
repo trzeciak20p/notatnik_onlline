f4960d31efa2d1f5dd73c504fb797c1ffc5774c4
trzeciak20p <91263438+trzeciak20p@users.noreply.github.com>
2022-04-25 13:09:08 +0200

    Add files via upload

--- editor.php
+++ editor.php
@@ -70,8 +70,7 @@
                 echo $_POST["canvas"];
 
                 $save = $_POST["canvas"];
-                // $f = fopen("data/" . $_SESSION["login"]. "/". $_POST["open_project"]. "/canvas1.jpg", "r");
-                $dir = "data/" . $_SESSION["login"]. "/ae/canvas1.txt"; //
+                $dir = "data/" . $_SESSION["login"]. "/". $_POST["open_project"]. "/canvas1.jpg";
                 echo $dir;
                 $f = fopen($dir, "w+");
                 fwrite($f, $_POST["canvas"]);
